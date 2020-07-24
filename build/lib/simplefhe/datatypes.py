20d42710e0557c1263e01c22138ff7bab1f012a0
Samuel Li <git@samuelj.li>
2020-07-24 07:36:32 -0400

    Added unary negation

--- build/lib/simplefhe/datatypes.py
+++ build/lib/simplefhe/datatypes.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from seal import Ciphertext, Plaintext
 
 import simplefhe
@@ -5,11 +7,16 @@ import simplefhe
 
 class EncryptedValue:
     def __init__(self, value):
+        if isinstance(value, EncryptedValue): value = value._ciphertext
         if not isinstance(value, Ciphertext):
-            value = simplefhe.encrypt(value)
+            value = simplefhe.encrypt(value)._ciphertext
 
         self._ciphertext = value
         self._mode = simplefhe._mode
+
+    @property
+    def _is_float(self):
+        return self._mode['type'] == 'float'
 
 
     def _binop(
@@ -36,32 +43,34 @@ class EncryptedValue:
         if isinstance(other, EncryptedValue):
             other = other._ciphertext
 
-
         # Must normalize floats to same scale before adding/subtracting
-        float_mode = (simplefhe._mode['type'] == 'float')
         evaluator = simplefhe._evaluator
 
         # If adding, we need to match scale and modulus
-        if float_mode and not _is_mult:
+        if self._is_float:
             target_scale = simplefhe._mode['default_scale']
-            self._ciphertext.scale(target_scale)
+            if not _is_mult:
+                self._ciphertext.scale(target_scale)
+
             parms = self._ciphertext.parms_id()
-            def normalize(x):
-                simplefhe._evaluator.mod_switch_to_inplace(x, parms)
+            def normalize(x, p=parms):
+                simplefhe._evaluator.mod_switch_to_inplace(x, p)
+
 
         # After each multiplication, we should relinearize
         def renormalize(x):
-            if float_mode and _is_mult:
-                evaluator.relinearize_inplace(x, simplefhe._relin_key)
-                evaluator.rescale_to_next_inplace(x)
+            if _is_mult:
+                evaluator.relinearize_inplace(x, simplefhe._relin_keys)
+                if self._is_float:
+                        evaluator.rescale_to_next_inplace(x)
 
-        # Determine type of other operand
+            # Determine type of other operand
         if not isinstance(other, Ciphertext):
             from simplefhe.encryptors import encode_item
-            if False:#plain_func is not None:
+            if plain_func is not None:
                 # Use plain_func for performance
                 pt = encode_item(other)
-                if float_mode and not _is_mult: normalize(pt)
+                if self._is_float: normalize(pt)
                 plain_func(self._ciphertext, pt, result)
                 renormalize(result)
                 return EncryptedValue(result)
@@ -70,9 +79,15 @@ class EncryptedValue:
                 other = simplefhe.encrypt(other)._ciphertext
 
         # If adding, we need to match scale and modulus
-        if float_mode and not _is_mult:
+        #if self._is_float and not _is_mult:
+        if self._is_float:
             other.scale(target_scale)
-            normalize(other)
+            try:
+                normalize(other)
+            except:
+                self._ciphertext.scale(target_scale)
+                normalize(self._ciphertext, p=other.parms_id())
+
 
         # Compute binary operation
         cipher_func(self._ciphertext, other, result)
@@ -111,6 +126,42 @@ class EncryptedValue:
     __rmul__ = __mul__
 
 
+    def __truediv__(self, other):
+        if not self._is_float:
+            raise NotImplementedError('Integer division is not implemented!')
+        if isinstance(other, int) or isinstance(other, float):
+            return self * (1/other)
+        else:
+            raise NotImplementedError('Only division by an unencrypted value is implemented!')
+
+    def square(self):
+        output = Ciphertext()
+        evaluator = simplefhe._evaluator
+        evaluator.square(self._ciphertext, output)
+        evaluator.relinearize_inplace(output, simplefhe._relin_keys)
+        if self._is_float:
+            evaluator.rescale_to_next_inplace(output)
+        return EncryptedValue(output)
+
+
+    def __pow__(self, other):
+        if isinstance(other, int) and other >= 0:
+            # Exponentiation by squaring
+            components = []
+            curr = self
+            bits = bin(other)[:1:-1]
+            for i, digit in enumerate(bits):
+                if digit == '1':
+                    components.append(curr)
+                if i < len(bits) - 1:
+                    curr = curr.square()
+            return smart_product(components)
+        else:
+            if self._is_float:
+                raise NotImplementedError('Fractional powers not yet implemented')
+            else:
+                raise TypeError('Only non-negative, unencrypted integer exponents are supported in integer mode!')
+
     def __repr__(self):
         type_string = self._mode['type']
         return f'<encrypted {type_string}>'
@@ -126,3 +177,17 @@ def load_encrypted_value(filepath: str) -> EncryptedValue:
     ciphertext = Ciphertext()
     ciphertext.load(simplefhe._context, filepath)
     return EncryptedValue(ciphertext)
+
+
+# Return the product of the given list.
+# A smart algorithm is used to conserve the noise budget.
+def smart_product(values: List[EncryptedValue]) -> EncryptedValue:
+    if not values: return simplefhe.encrypt(1)
+    if len(values) == 1: return values[0]
+
+    new = []
+    for i in range(len(values) // 2):
+        new.append(values[2*i] * values[2*i+1])
+    if len(values) % 2 == 1:
+        new.append(values[-1])
+    return smart_product(new)
--- build/lib/simplefhe/decryptors.py
+++ build/lib/simplefhe/decryptors.py
@@ -5,14 +5,23 @@ import simplefhe
 
 def decrypt(item):
     decryptor = simplefhe._decryptor
-    if decryptor is None:
+
+    if simplefhe._private_key is None:
         raise ValueError('Private key has not been set. Decryption not possible.')
+
+    if simplefhe._relin_keys is None:
+        raise ValueError('Relinearization keys have not been set. Decryption not possible.')
 
     result = Plaintext()
     decryptor.decrypt(item._ciphertext, result)
 
     mode = item._mode
     if mode['type'] == 'int':
+        if decryptor.invariant_noise_budget(item._ciphertext) == 0:
+            raise ValueError(
+                'The noise budget has been exhausted.'
+                + ' Try calling `simplefhe.initialize` with a larger `poly_modulus_degree` or a smaller `max_int`.'
+            )
         result = result.to_string()
         result = int(result, 16) 
         if result > mode['modulus'] // 2:
